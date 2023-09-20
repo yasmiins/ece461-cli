9f3657aad47c3410340cd711bdf2e2ff050793dc
Dulani Wijayarathne <dulaninw@pal-10-184-42-98.itap.purdue.edu>
2023-09-20 12:11:31 -0400

    added license-verifier

--- src/services/license-verifier.ts
+++ src/services/license-verifier.ts
@@ -8,12 +8,10 @@ import * as crypto from 'crypto';
 @injectable()
 export class LicenseVerifier {
     public async verifyLicense(url: string): Promise<boolean> {
-        // Ensure the URL starts with https://
         if (!url.startsWith('https://')) {
             url = `https://${url}`;
         }
         
-        // Create a unique identifier for this URL to use as a directory name
         const uniqueId = crypto.createHash('md5').update(url).digest('hex');
         const dirPath = `./tmp_repo_${uniqueId}`;
         
@@ -27,20 +25,23 @@ export class LicenseVerifier {
                 depth: 1,
             });
 
-            let licenseText: string | null = null;
             let readmeText: string | null = null;
+            let licenseFileText: string | null = null;
             
-            
+            // Read README.md
             try {
                 readmeText = await fsPromises.readFile(join(dirPath, 'README.md'), 'utf8');
             } catch (error) {
-                throw error;
+                // Handle error if needed
             }
             
+            // Read LICENSE file if available
+            try {
+                licenseFileText = await fsPromises.readFile(join(dirPath, 'LICENSE'), 'utf8');
+            } catch (error) {
+                // Handle error if needed
+            }
 
-            const licenseInReadmeMatch = readmeText?.match(/##\s*License[\s\S]+?((##|$))/i);
-            const licenseInReadme = licenseInReadmeMatch ? licenseInReadmeMatch[0] : null;
-            
             const compatibleLicenses = [
                 'MIT', 'BSD', 'Apache 2.0', 'Zlib',
                 'ISC', 'Artistic 2.0', 'Python Software Foundation',
@@ -48,15 +49,19 @@ export class LicenseVerifier {
                 'GNU General Public License v2 or later',
                 'GNU Lesser General Public License v2.1 or later'
             ];
-            
-            for (const lic of compatibleLicenses) {
-                const licenseRegExp = new RegExp(lic, 'i');
-                if ((licenseText && licenseRegExp.test(licenseText)) || (licenseInReadme && licenseRegExp.test(licenseInReadme))) {
-                    return true;
-                }
-            }
 
-            return false;
+            const searchInText = (text: string | null) => {
+                for (const lic of compatibleLicenses) {
+                    const licenseRegExp = new RegExp(lic, 'i');
+                    if (text && licenseRegExp.test(text)) {
+                        return true;
+                    }
+                }
+                return false;
+            };
+
+            // Check for licenses in README and LICENSE
+            return searchInText(readmeText) || searchInText(licenseFileText);
 
         } catch (error) {
             console.error('An error occurred:', error);
